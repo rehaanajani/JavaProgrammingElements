badd10e2cac361893870995198f88fa112b2ff45
Rehaan Ajani <rehaanajani123@gmail.com>
2026-02-22 00:49:26 +0530

    Add files via upload

---
+++ Level1Practice.java
@@ -0,0 +1,16 @@
+public class Level1Practice {
+
+    public static void main(String[] args) {
+
+        int birthYear = 2000;
+        int currentYear = 2024;
+        int age = currentYear - birthYear;
+
+        System.out.println("Harry's age in 2024 is " + age);
+
+        int maths = 94, physics = 95, chemistry = 96;
+        double average = (maths + physics + chemistry) / 3.0;
+
+        System.out.println("Sam’s average mark in PCM is " + average);
+    }
+}
